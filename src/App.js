a1ec9430868bc750071fc96ddbad9ba623d4f6db
arashesmailian <arashesmailian@gmail.com>
2022-06-12 17:04:50 +0430

    deleting no needed files

--- src/App.js
+++ src/App.js
@@ -1,40 +1,11 @@
 import React, {Component} from 'react';
-// import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
-// import HTMLForm from './HTMLForm';
-// import ControlledForm from './ControlledForm';
 import ReduxForm from './ReduxForm';
-// import FinalForm from './FinalForm';
-// import HTMLFormSummary from './summary/HTMLForm';
-// import ControlledFormSummary from './summary/ControlledForm';
-// import ReduxFormSummary from './summary/ReduxForm';
-// import FinalFormSummary from './summary/FinalForm';
 import {Provider} from 'react-redux';
 import {createStore, combineReducers} from 'redux';
 import {reducer as formReducer} from 'redux-form';
 import './App.css';
 import dataReducer from './jsonData';
 
-// const Index = () => (
-//   <div>
-//     <h2>Mini-Course Overview</h2>
-//     <nav>
-//       <ol>
-//         <li>
-//           <Link to="/html-form/">Native HTML-like Form</Link>
-//         </li>
-//         <li>
-//           <Link to="/controlled-form/">Controlled Form</Link>
-//         </li>
-//         <li>
-//           <Link to="/redux-form/">Redux Form</Link>
-//         </li>
-//         <li>
-//           <Link to="/final-form/">Final Form</Link>
-//         </li>
-//       </ol>
-//     </nav>
-//   </div>
-// );
 
 const reducers = combineReducers({
   form: formReducer,
