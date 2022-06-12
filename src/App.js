2807505f18b6cf282c61dd9a1d04e050f4d3eabc
arashesmailian <arashesmailian@gmail.com>
2022-06-12 16:50:52 +0430

    cleaning

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
-import HTMLForm from './HTMLForm';
-import ControlledForm from './ControlledForm';
+// import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+// import HTMLForm from './HTMLForm';
+// import ControlledForm from './ControlledForm';
 import ReduxForm from './ReduxForm';
-import FinalForm from './FinalForm';
-import HTMLFormSummary from './summary/HTMLForm';
-import ControlledFormSummary from './summary/ControlledForm';
-import ReduxFormSummary from './summary/ReduxForm';
-import FinalFormSummary from './summary/FinalForm';
+// import FinalForm from './FinalForm';
+// import HTMLFormSummary from './summary/HTMLForm';
+// import ControlledFormSummary from './summary/ControlledForm';
+// import ReduxFormSummary from './summary/ReduxForm';
+// import FinalFormSummary from './summary/FinalForm';
 import {Provider} from 'react-redux';
 import {createStore, combineReducers} from 'redux';
 import {reducer as formReducer} from 'redux-form';
 import './App.css';
 import dataReducer from './jsonData';
 
-const Index = () => (
-  <div>
-    <h2>Mini-Course Overview</h2>
-    <nav>
-      <ol>
-        <li>
-          <Link to="/html-form/">Native HTML-like Form</Link>
-        </li>
-        <li>
-          <Link to="/controlled-form/">Controlled Form</Link>
-        </li>
-        <li>
-          <Link to="/redux-form/">Redux Form</Link>
-        </li>
-        <li>
-          <Link to="/final-form/">Final Form</Link>
-        </li>
-      </ol>
-    </nav>
-  </div>
-);
+// const Index = () => (
+//   <div>
+//     <h2>Mini-Course Overview</h2>
+//     <nav>
+//       <ol>
+//         <li>
+//           <Link to="/html-form/">Native HTML-like Form</Link>
+//         </li>
+//         <li>
+//           <Link to="/controlled-form/">Controlled Form</Link>
+//         </li>
+//         <li>
+//           <Link to="/redux-form/">Redux Form</Link>
+//         </li>
+//         <li>
+//           <Link to="/final-form/">Final Form</Link>
+//         </li>
+//       </ol>
+//     </nav>
+//   </div>
+// );
 
 const reducers = combineReducers({
   form: formReducer,
@@ -49,33 +49,9 @@ class App extends Component {
       <Provider store={store}>
         <div className="App">
           <header className="header">
-            <span className="logo">kubucation</span>
-            <span className="title">4 Ways to manage form state in React</span>
+            <span className="logo">testForm</span>
           </header>
-          <Router>
-            <div style={{padding: 20}}>
-              <Switch>
-                <Route path="/" exact component={Index} />
-                <Route path="/html-form/summary" component={HTMLFormSummary} />
-                <Route
-                  path="/controlled-form/summary"
-                  component={ControlledFormSummary}
-                />
-                <Route
-                  path="/redux-form/summary"
-                  component={ReduxFormSummary}
-                />
-                <Route
-                  path="/final-form/summary"
-                  component={FinalFormSummary}
-                />
-                <Route path="/html-form/" component={HTMLForm} />
-                <Route path="/controlled-form/" component={ControlledForm} />
-                <Route path="/redux-form/" component={ReduxForm} />
-                <Route path="/final-form/" component={FinalForm} />
-              </Switch>
-            </div>
-          </Router>
+          <ReduxForm />
         </div>
       </Provider>
     );
